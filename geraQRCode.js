1dadfb5200c76ebf4b8dead9903c43dcefa1f3ad
cemeterydriiver <ashlynierow@gmail.com>
2023-04-21 22:26:35 -0300

    scan

--- geraQRCode.js
+++
@@ -1,9 +0,0 @@
-new QRCode(document.getElementById('qrcode'), {
-    text: 'https://github.com/cemeterydriiver',
-    width: 300,
-    height: 300,
-    colorDark: '#ff0000',
-    colorLight: '#ffffff',
-    correctLevel: QRCode.CorrectLevel.H,
-  });
-  
